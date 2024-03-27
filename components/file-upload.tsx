eaebaa7fdcaf99840c29648ad081f9c58f3a2f48
harry <harrybsteele@gmail.com>
2024-03-27 18:13:01 -0400

    working on upload thing

---
+++ components/file-upload.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { UploadDropzone, UploadButton } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+
+type FileUploadProps = {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+};
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => onChange(res?.[0].url)}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
